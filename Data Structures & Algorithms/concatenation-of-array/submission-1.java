819f535e01f72c52546952c9757d1aef3c3e2ed1
chandrani09sd <chandrani09.sd@gmail.com>
2026-09-14 13:59:54 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int i;
+        int[] ans= new int[2*n];
+        System.arraycopy(nums, 0, ans, 0, nums.length);
+        System.arraycopy(nums, 0, ans, nums.length, nums.length);
+        return(ans);
+    }
+}
